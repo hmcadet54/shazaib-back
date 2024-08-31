26ba38ed650ab43b282633461d4daf8c8ae71530
Abdullah Shafiq <as053266@gmail.com>
2024-08-31 21:20:32 +0500

    asdasd

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ import cors from "cors"
 const app = express()
 const PORT = process.env.PORT || 5000
 
-
-
 // mongo db connection
 const uri = "mongodb+srv://elderbrotherwedding:A_hd3LZtd.PcXBQ@crud.u8eekys.mongodb.net/auth-api?retryWrites=true&w=majority&appName=crud"
 
@@ -16,18 +14,30 @@ const uri = "mongodb+srv://elderbrotherwedding:A_hd3LZtd.PcXBQ@crud.u8eekys.mong
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({ origin: "*" }))
+
 //routes
+app.use('/', route)
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 // connection to the database
-mongoose.connect(uri)
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Database connected")
-
-        app.listen(PORT, () => {
-            console.log(`Server is running on PORT ${PORT}`)
-        })
-
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+        console.error("Database connection error:", err)
+    })
 
-app.use(route)
+// Start the server
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT ${PORT}`)
+    })
+}
+
+export default app
